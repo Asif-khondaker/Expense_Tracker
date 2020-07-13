a275a6af4e28c13b3789404ca0fddd2c00e98740
Ali Hassan Faruk Asif <asifkhondaker004@gmail.com>
2020-07-13 15:36:36 +0600

    Updates

---
+++ precache-manifest.30fd0e323578b84803ff86bd44535ee7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7049022c4512cf81565b40e50f805bd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0de7b0552f4f7573f81",
+    "url": "/static/css/main.ba037b4b.chunk.css"
+  },
+  {
+    "revision": "64629b05b432c44e1bd6",
+    "url": "/static/js/2.4599387a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4599387a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0de7b0552f4f7573f81",
+    "url": "/static/js/main.7fcae238.chunk.js"
+  },
+  {
+    "revision": "1eede93b43bca28572ca",
+    "url": "/static/js/runtime-main.399baf1c.js"
+  }
+]);
